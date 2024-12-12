cadd062a2c4409a7a32d63da9bea411da366dc16
ahernandez <alberto@44t.tech>
2024-12-12 17:47:52 +0100

    Add Aliases and FMT

--- versions.tf
+++ versions.tf
@@ -2,8 +2,10 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = "~> 5.80.0"
-      configuration_aliases = ["ecr"]
+      version               = ">= 5.80.0"
+      configuration_aliases = [aws.ecr]
     }
   }
+
+  required_version = ">= 1.10.1"
 }
